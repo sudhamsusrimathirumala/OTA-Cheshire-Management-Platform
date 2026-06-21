1f34e779a440ef2dcf8ae1aacb27c364b55ba2a3
Sudhamsu <academyoffice79@gmail.com>
2026-06-21 16:50:04 -0400

    create initial ota app shell

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const OTAApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class OTAApp extends StatelessWidget {
+  const OTAApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'OTA Cheshire',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo'),
-        ),
-        body: Center(
-          child: Text('Hell'),
-        ),
-      ),
+      home: const LoginScreen(),
     );
   }
 }
 
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Olympic Taekwondo Academy'),
+      ),
+      body: const Center(
+        child: Text('Login Screen'),
+      ),
+    );
+  }
+}
