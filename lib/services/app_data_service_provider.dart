ade0e660dd86b0e3fd30f4695a473a820bb59756
Sudhamsu <academyoffice79@gmail.com>
2026-07-07 11:06:06 -0400

    Implement live Firestore schedule with streaming updates

--- lib/services/app_data_service_provider.dart
+++ lib/services/app_data_service_provider.dart
@@ -2,7 +2,7 @@ import 'app_data_service.dart';
 import 'firebase/firebase_app_data_service.dart';
 import 'mock_app_data_service.dart';
 
-const bool useFirebase = false;
+const bool useFirebase = true;
 
 final AppDataService appDataService = useFirebase
     ? FirebaseAppDataService()
